15fb7a7697e28c42f5bde87a6da99c2501cc2144
Sankeerth <finance.san2292@gmail.com>
2021-05-21 04:49:56 +0530

    Test cloud run deployment for server

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from multiprocessing import Process
 from background_task import notifyAvailabilityByEmail
 
 def runApp():
-    uvicorn.run('main:app', port=4200, reload=True, reload_dirs=['templates'])
+    uvicorn.run('main:app', reload=True, reload_dirs=['templates'])
 
 if __name__ == '__main__':
     appProcess = Process(target=runApp)
