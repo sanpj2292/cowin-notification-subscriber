90e14d280dcae937751300df6ec13431608051a2
Sankeerth <finance.san2292@gmail.com>
2021-05-21 19:16:30 +0530

    spell mistake correction

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from background_task import notifyAvailabilityByEmail
 import os
 
 def runApp():
-    uvicorn.run('main:app', host='0.0.0.0', port=int(os.getnev('PORT')) or 4200, reload=True, reload_dirs=['templates'])
+    uvicorn.run('main:app', host='0.0.0.0', port=int(os.getenv('PORT')) or 4200, reload=True, reload_dirs=['templates'])
 
 if __name__ == '__main__':
     appProcess = Process(target=runApp)
